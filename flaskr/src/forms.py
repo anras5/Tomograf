329a6912e40d8d07553b403c7655406d9eea8ed1
anras5 <anras1filip@gmail.com>
2023-04-07 10:35:45 +0200

    Add spinner

--- flaskr/src/forms.py
+++ flaskr/src/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
 
 
 class PhotoForm(FlaskForm):
-    photo = FileField("Prześlij swój plik", validators=[FileRequired()])
+    photo = FileField("Prześlij swój plik",
+                      validators=[FileRequired(), FileAllowed(['jpg', 'png', 'dcm'], 'Images or DICOM only!')])
     submit = SubmitField("Prześlij")
